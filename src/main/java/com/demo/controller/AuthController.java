e6cd3c2c7a2b95c325d453cc20e5c27db5f119f7
Vijay Bighane <vijaybighane12@gmail.com>
2024-07-07 23:16:31 +0530

    added new method in auth controller with location details

--- src/main/java/com/demo/controller/AuthController.java
+++ src/main/java/com/demo/controller/AuthController.java
@@ -1,7 +1,25 @@
 package com.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
-public void AuthContoller() {
-	// this is demo auth controller.
+
+    @GetMapping("/getLocation")
+    public String getLocation(){
+        return "Bangalore";
+    }
+
+    @PostMapping ("/postLocation")
+    public String postLocation(){
+        return "Bangalore";
+    }
+    @PutMapping("/updateLocation")
+    public String updateLocation(){
+        return "Bangalore";
+    }
+
 }
-}
+
