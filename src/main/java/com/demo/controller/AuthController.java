02bbd55f2f40d6047e189a7756fd24620e119e83
Vijay Bighane <vijaybighane12@gmail.com>
2024-07-08 00:29:52 +0530

    latest commit into github

--- src/main/java/com/demo/controller/AuthController.java
+++ src/main/java/com/demo/controller/AuthController.java
@@ -1,5 +1,10 @@
 package com.demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 public class AuthController {
 
     @GetMapping("/getLocation")
@@ -7,7 +12,7 @@ public class AuthController {
         return "Bangalore";
     }
 
-    @PostMapping ("/postLocation")
+    @PostMapping("/postLocation")
     public String postLocation(){
         return "Bangalore";
     }
@@ -18,5 +23,10 @@ public class AuthController {
 public void AuthContoller() {
 	// this is demo auth controller.
 	System.out.println("Authentication and Authorization provider");
+}
+
+@DeleteMapping("/DeleteId")
+public String deleteId(){
+return "deleted the id successfully";
 }
 }
